041ed2cba4bc12fcfddfc63f5f2f383d6e3cda40
Angel-mk <angelkristina36@gmail.com>
2025-11-05 13:42:03 +0400

    Initial commit

---
+++ authorize.py
@@ -0,0 +1,9 @@
+import bcrypt
+def hash_password(plain_text_pass):
+    pass_bytes = plain_text_pass.encode('utf-8')
+    salt = bcrypt.gensalt()
+    hashed_pass = bcrypt.hashpw(pass_bytes, salt)
+    return hashed_pass
+passwd = "secret"
+hashed_pass = hash_password(passwd)
+print(f'Password:{passwd} Hash : {str(hashed_pass)}')
