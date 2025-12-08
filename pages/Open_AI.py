69abb99076f3eb3ee14f50c1f1e76dd6261d32bb
Angel-mk <angelkristina36@gmail.com>
2025-12-08 13:29:48 +0400

    Initial commit

--- pages/Open_AI.py
+++ pages/Open_AI.py
@@ -1,4 +1,6 @@
 from openai import OpenAI
+from pip._internal.cli.cmdoptions import python
+
 # Initialize the OpenAI client with your API key
 client = OpenAI(api_key='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')
 # Make a simple API call
